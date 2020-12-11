63911089b79a6bd8c4b9ccd9b8815b68312505d0
Gio Eufshi <dixtosa@gmail.com>
2020-12-11 18:10:57 +0400

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,65 @@
+/*
+ID: dixtosa1
+PROG: milk2
+LANG: C++11
+*/
+#pragma comment(linker, "/STACK:16777216")
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <stdio.h>
+#include <cstdlib>
+#include <stdlib.h>
+#include <string>
+//#include <string.h>
+#include <list>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <iomanip>
+#include <queue>
+#include <deque>
+#include <set>
+#include <stack>
+#include <sstream>
+#include <assert.h>
+#include <functional>
+
+typedef long long ll;
+typedef std::pair<ll, ll> pii;
+#define ALL(x)           (x).begin(), (x).end()
+#define forn(N)          for(long long i = 0; i < (long long)N; i++)
+#define fornj(N)         for(long long j = 0; j < (long long)N; j++)
+#define fornk(N)         for(long long k = 0; k < (long long)N; k++)
+#define foreach(c,itr)   for(auto itr=(c).begin();itr!=(c).end();itr++)
+#define PI 3.1415926535897932384626433
+#define LINF (1LL<<60)
+#define INF (1<<30)
+#define v vector
+#define endl '\n'
+//printf("\n\ntime-%.3lf", clock()*1e-3);
+//g++ file.cpp -D_DEBUG; ./a.out ;cat output.txt
+//#define File "FILENAME"
+using namespace std;
+void handleInput() {
+#if defined(_DEBUG) || defined(_RELEASE)
+	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#elif defined(File)
+	freopen(File".in", "r", stdin); freopen(File".out", "w", stdout);
+#else
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+#endif
+}
+//YOUR CODE STARTS HERE
+
+
+
+
+int main()
+{
+	handleInput();
+
+	return 0;
+}
